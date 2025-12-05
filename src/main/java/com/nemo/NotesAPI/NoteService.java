fd0cf0f811848da5d07f5e8bd5dbb655338fe3bc
Richard Peczanek <pecz.rich@gmail.com>
2025-12-05 11:18:13 +0100

    Note created, need to do NoteService

---
+++ src/main/java/com/nemo/NotesAPI/NoteService.java
@@ -0,0 +1,12 @@
+package com.nemo.NotesAPI;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
+public class NoteService {
+
+
+}
